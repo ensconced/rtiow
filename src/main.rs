fd5190db99ae30d9e1cb04df0bf1dcf3dfcb7eff
Joe Barnett <joebarnett876@gmail.com>
2021-12-12 21:53:01 +0000

    wip (2.3)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::{self, Write};
+
 const IMAGE_WIDTH: u32 = 256;
 const IMAGE_HEIGHT: u32 = 256;
 const MAX_COLOR: f32 = 255.0;
@@ -7,12 +9,19 @@ fn divide(num: u32, denom: u32) -> f32 {
     num as f32 / denom as f32
 }
 
+fn display_progress() {
+    let mut stderr = io::stderr();
+    stderr.write_all(b"hello world")
+}
+
 fn main() {
     println!("P3"); // means this is an RGB color image in ASCII
     println!("{} {}", IMAGE_WIDTH, IMAGE_HEIGHT);
     println!("{}", MAX_COLOR);
 
     for row in 0..IMAGE_HEIGHT {
+        display_progress();
+
         for col in 0..IMAGE_WIDTH {
             let red_level = divide(col, IMAGE_WIDTH - 1);
             let green_level = 1.0 - divide(row, IMAGE_HEIGHT - 1);
