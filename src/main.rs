fa742b1de08b7a19622be0d4e447bc9baa4a0056
Joe Barnett <joebarnett876@gmail.com>
2021-12-13 08:31:00 +0000

    add progress indicator

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::io::{self, Write};
+use std::thread::sleep;
+use std::time;
 
 const IMAGE_WIDTH: u32 = 256;
 const IMAGE_HEIGHT: u32 = 256;
@@ -9,9 +11,21 @@ fn divide(num: u32, denom: u32) -> f32 {
     num as f32 / denom as f32
 }
 
-fn display_progress() {
+fn display_progress(row: u32) {
+    let scanlines_remaining = IMAGE_HEIGHT - row;
     let mut stderr = io::stderr();
-    stderr.write_all(b"hello world")
+    if row > 0 {
+        stderr
+            .write_all(b"\x1B[2K") // clear line and return cursor to start
+            .expect("failed to write to stderr");
+        stderr
+            .write_all(b"\r") // clear line and return cursor to start
+            .expect("failed to write to stderr");
+    }
+    let msg = format!("scanlines remaining: {}", scanlines_remaining);
+    stderr
+        .write_all(msg.as_bytes())
+        .expect("failed to write to stderr");
 }
 
 fn main() {
@@ -20,7 +34,8 @@ fn main() {
     println!("{}", MAX_COLOR);
 
     for row in 0..IMAGE_HEIGHT {
-        display_progress();
+        sleep(time::Duration::from_millis(100));
+        display_progress(row);
 
         for col in 0..IMAGE_WIDTH {
             let red_level = divide(col, IMAGE_WIDTH - 1);
@@ -33,4 +48,6 @@ fn main() {
             println!("{} {} {}", r, g, b);
         }
     }
+    let mut stderr = io::stderr();
+    stderr.write_all(b"\n").expect("oh no");
 }
